1d338ebef10b7c318a4105a13b3b2ef7b5f9f2c2
Andrey <v.andrei.dmitrievich@gmail.com>
2024-05-27 22:23:35 +0300

    merge2

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import MessageButton from './components/MessageButton';
 import './components/CatalogIndex'
 import CatlogIndex from './components/CatalogIndex';
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <MessageButton />
         <CatlogIndex/>
       </header>
     </div>
