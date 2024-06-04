84e4bea837d80d00d82b2b04de816fba02bce9c5
Madzhuga Aleksandra <149695707+MadzhugaAlexa@users.noreply.github.com>
2024-06-04 18:08:50 +0400

    Фронтенд для категорий каталога (#11)
    * Фронтенд для выбора категорий

--- front/src/components/CatalogIndex.js
+++ front/src/components/CatalogIndex.js
@@ -1,31 +1,72 @@
 import React, { useState, useEffect } from 'react';
 
 import Dish from './Dish'
+import Category from './Category'
 
 function CatalogIndex() {
   const [dishes, setDishes] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [categoriesFilter, setCategoriesFilter] = useState([])
 
   useEffect(() => {
-    fetch('http://localhost/api/v1/catalog')
+    let url = 'http://localhost/api/v1/catalog'
+    if (categoriesFilter.length > 0) {
+      url = url + '?';
+    }
+    console.log("mama: ", categoriesFilter)
+    categoriesFilter.forEach((id) => {
+      url = url + 'categories[]=' + id
+    })
+
+    fetch(url)
          .then((response) => response.json())
          .then((data) => {
-            console.log(data);
             setDishes(data.Dishes);
+            
+            let cats = [];
+
+            data.Dishes.forEach((dish) => {
+              dish.categories.forEach((category) => {
+                for (let i = 0; i < cats.length; i++) {
+                  if (cats[i].id === category.id) {
+                    return
+                  }
+                }
+  
+                cats.push(category)
+              });
+            });
+
+            setCategories(cats);
          })
          .catch((err) => {
             console.log(err.message);
          });
-  }, []);
+  }, [categoriesFilter]);
 
   let dishesList = [];
 
   dishes.forEach((dish, index) => {
     dishesList.push(<Dish dish={dish}/>)
-  })
+  });
+
+  let categoriesList = [];
+
+  function toggleCategory(id) {
+    setCategoriesFilter([...categoriesFilter, id])
+  }
+
+  categories.forEach((category, index) => {
+    categoriesList.push(<Category category={category} toggleCategory={toggleCategory} />)
+  });
 
   return (
     <div className="CatalogIndex">
+      
       <h1>Каталог</h1>
+        <h2>Категории</h2>
+        { categoriesList }
+        
         { dishesList }
       {}
     </div>
