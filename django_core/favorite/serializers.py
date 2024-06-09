0fcaecb60c617288f5e300a388a534e2b10c51a8
Andrey <v.andrei.dmitrievich@gmail.com>
2024-06-09 11:49:33 +0300

    basic favorite

--- django_core/favorite/serializers.py
+++ django_core/favorite/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 
+from api.models import CustomUser
 from .models import Favorite
 
 
 class FavoriteSerializer(serializers.Serializer):
-    user = serializers.PrimaryKeyRelatedField(queryset=Favorite.objects.all())
+    user = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all())
     dishes = serializers.IntegerField()
 
     def create(self, validated_data):
