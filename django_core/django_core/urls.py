97071cbbf7586366f8dec2c28d2b405ceed90ff0
Andrey <v.andrei.dmitrievich@gmail.com>
2024-06-15 10:27:05 +0300

    v1 swagger

--- django_core/django_core/urls.py
+++ django_core/django_core/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/', include('inform.urls')),
-    path('api/', include('favorite.urls'))
+    path('api/', include('favorite.urls')),
+    path('swagger-ui/', TemplateView.as_view(template_name='swagger-ui.html'))
 ]
