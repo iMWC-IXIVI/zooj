396df6fcd7cd3f53e2a7b1b0092a8d44cba25864
Egorro <egorro@inbox.ru>
2024-06-19 23:11:44 +0300

    Add positive API tests v1

---
+++ qa/config.py
@@ -0,0 +1,13 @@
+import uuid
+
+host = '127.0.0.1'
+
+db_name = 'django'
+user = 'django'
+password = 'django'
+port = 5433
+
+myemail = 'egorro@inbox.ru'
+
+Anonymous_Uuid = 'f26e4f69-b2d5-4531-b2b2-108e85d9a17d'
+ANON_UUID = 'f26e4f69-b2d5-4531-b2b2-108e85d9a17d'
