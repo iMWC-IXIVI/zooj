97f216992d8c96542fe426a4d8d17edaf8ce892e
Ivan Gubkin <iv.gubkin19988@gmail.com>
2024-06-21 21:42:02 +0300

    fix

--- front/src/components/Buttons/CloseButton/CloseButton.jsx
+++ front/src/components/Buttons/CloseButton/CloseButton.jsx
@@ -3,7 +3,7 @@ import classes from "./CloseButton.module.scss"
 
 export default function CloseButton({onClick }) {
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={classes.close_button} onClick={onClick}>
       <SvgSelector name="close"/>
     </button>
   );
