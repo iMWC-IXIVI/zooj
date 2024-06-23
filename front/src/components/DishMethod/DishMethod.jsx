d84d0fd9c24fa555b1d28e03eb7325be4782193a
Alexa <madzhav@me.com>
2024-06-23 22:48:57 +0400

    Фронт для пол-тарелок

--- front/src/components/DishMethod/DishMethod.jsx
+++ front/src/components/DishMethod/DishMethod.jsx
@@ -1,9 +1,75 @@
-
+import { useState, useEffect } from "react";
+import classes from "./DishMethod.module.scss";
+import HalfDish from "../HalfDish/HalfDish";
 
 export default function DishMethod() {
-    return(
-        <div>
-            DishMethod
+  const [dishes, setDishes] = useState([]);
+
+  useEffect(() => {
+    let url = "http://localhost/api/v1/catalog";
+
+    let params = ['half_dishes=true'];
+
+    if (params.length > 0) {
+      url = url + "?" + params.join("&");
+    }
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        let dishes = [];
+
+        data.dishes.forEach((dish) => {
+          dishes.push(dish)
+        });
+
+        setDishes(dishes);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  const [currentDish, setCurrentDish] = useState({half_dishes: []})
+  const [currentLeftDish, setCurrentLeftDish] = useState({})
+  const [currentRightDish, setCurrentRightDish] = useState({})
+
+  const setLeftDish = function(halfDish) {
+    setCurrentDish({half_dishes: [{...halfDish}, {...currentRightDish}]})
+    setCurrentLeftDish(halfDish)
+  }
+
+  const setRightDish = function(halfDish) {
+    setCurrentDish({half_dishes: [{...currentLeftDish}, {...halfDish}]})
+    setCurrentRightDish(halfDish)
+  }
+  
+  let dishesList = [];
+  dishes.forEach((dish) => dishesList.push(
+    <HalfDish dish={dish} setLeftDish={setLeftDish} setRightDish={setRightDish} leftDishId={currentLeftDish.id} rightDishId={currentRightDish.id} />
+  ));
+
+  return (
+    <div>
+      <div className={classes.breakfast}>
+        <div className={classes.currentDish}>
+          <h1>Метод тарелки</h1>
+          <HalfDish dish={currentDish} />
+
+
+          <h2>Ваша тарелка</h2>
+          <div>РСК: {(currentRightDish.kcal || 0) + (currentLeftDish.kcal || 0)} </div>
+          <div>Белки: {(currentRightDish.proteins || 0) + (currentLeftDish.proteins || 0)} </div>
+          <div>Жиры: {(currentRightDish.fats || 0) + (currentLeftDish.fats || 0)} </div>
+          <div>Углеводы: {(currentRightDish.carbos || 0) + (currentLeftDish.carbos || 0)} </div>
+          
         </div>
-    )
+        <div className={classes.halfDishes}>
+          <div className={classes.halfdishIndex}>{dishesList}</div>
+        </div>
+
+        <div className={classes.clearFloat}></div>
+      </div>
+    </div>
+  );
 }
