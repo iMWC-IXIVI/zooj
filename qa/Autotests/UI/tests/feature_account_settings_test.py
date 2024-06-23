fd2e943dee8955a6abafd9447aa1840d5306e72c
Egorro <egorro@inbox.ru>
2024-06-24 02:34:03 +0300

    Refactor

--- qa/Autotests/UI/tests/feature_account_settings_test.py
+++ qa/Autotests/UI/tests/feature_account_settings_test.py
@@ -4,11 +4,11 @@ from base.base_test import BaseTest
 
 class TestAccountSettings(BaseTest):
 
-    @pytest.mark.smoke
-    def test_enter_info(self):
-        self.account_page.open()
-        self.account_page.enter_FIO(f'Test name {random.randint(1, 100)}')
-        self.account_page.enter_phone_number(random.randint(9000000000, 9999999999))
+    # @pytest.mark.smoke
+    # def test_enter_info(self):
+    #     self.account_page.open()
+    #     self.account_page.enter_FIO(f'Test name {random.randint(1, 100)}')
+    #     self.account_page.enter_phone_number(random.randint(9000000000, 9999999999))
 
     @pytest.mark.smoke
     def test_link_work(self):
